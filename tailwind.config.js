4f6e27e2224a41bcdbe99e7b4d7781c8e6267d46
Marek Včelák <vcelak.marek@gmail.com>
2024-10-04 23:16:51 +0200

    fix eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+import forms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        purple: "var(--purple)",
+        "purple-hover": "var(--purple-hover)",
+        red: "var(--red)",
+        "red-hover": "var(--red-hover)",
+        "dark-blue": "var(--dark-blue)",
+        blue: "var(--blue)",
+        "light-blue": "var(--light-blue)",
+        "light-grey": "var(--light-grey)",
+        grey: "var(--grey)",
+        black: "var(--black)",
+        black2: "var(--black2)",
+        "light-bg": "var(--light-bg)",
+      },
+      screens: {
+        md: { max: "900px" },
+        sm: { max: "600px" },
+      },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [forms],
+}
