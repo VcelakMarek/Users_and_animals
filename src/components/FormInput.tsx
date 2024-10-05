b34370c46aca1135e5f2285a8ba80150cbd8dbe7
Marek Včelák <vcelak.marek@gmail.com>
2024-10-05 22:02:06 +0200

    create FormInput.tsx

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,66 @@
+import { Field } from "react-final-form";
+
+type Size = "small" | "large";
+
+type Props = {
+  id: string;
+  size?: Size;
+  inputName?: string;
+  inputType?: "text" | "number" | "select";
+  inputPlaceholder?: string;
+  selectValues?: string[];
+};
+
+const inputSize: { [key in Size]: string } = {
+  small: "w-[120px]",
+  large: "md:w-[250px] w-[504px]",
+};
+
+const FormInput = ({
+  size = "large",
+  inputName,
+  id,
+  inputType = "text",
+  inputPlaceholder,
+  selectValues,
+}: Props) => {
+  if (inputType === "select") {
+    return (
+      <label htmlFor={id}>
+        <h2>{inputName}</h2>
+        <Field id={id} name={id} component="select" className={inputSize[size]}>
+          {selectValues?.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </Field>
+      </label>
+    );
+  }
+
+  return (
+    <label htmlFor={id}>
+      <Field name={id}>
+        {({ input }) => (
+          <div>
+            <div className="flex justify-between">
+              <h2>{inputName}</h2>
+            </div>
+            <input
+              {...input}
+              type={inputType}
+              className={`${inputSize[size]} `}
+              placeholder={inputPlaceholder}
+              onChange={(e) => {
+                input.onChange(e); //final-form's onChange
+              }}
+            />
+          </div>
+        )}
+      </Field>
+    </label>
+  );
+};
+
+export default FormInput;
