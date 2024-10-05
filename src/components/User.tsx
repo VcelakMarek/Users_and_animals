b0c798e0d0b64868f1d9059b819e5b0eddbc1f6c
Marek Včelák <vcelak.marek@gmail.com>
2024-10-05 21:58:28 +0200

    create User.tsx

---
+++ src/components/User.tsx
@@ -0,0 +1,33 @@
+import Button from "components/Button";
+import AccessStatus from "components/AccessStatus";
+import type { User } from "types/UserType";
+
+type Props = {
+  userData: User;
+};
+
+const User = ({ userData }: Props) => {
+  const handleDelete = () => {
+    // userDelete()
+  };
+
+  return (
+    <div className="mb-4 flex h-[72px] w-[65%] items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px] sm:h-[300px] sm:w-11/12 md:h-[200px] md:flex-wrap">
+      <h4 className="basis-3/12 text-start">{userData.name}</h4>
+      <h3 className="basis-1/12">{userData.gender}</h3>
+
+      <AccessStatus isBanned={userData.banned} />
+
+      <div className="flex basis-2/12 items-center gap-4">
+        <Button color="grey" link={`/list/edit/${userData.id}`}>
+          Edit
+        </Button>
+        <Button color="red" onClick={handleDelete}>
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default User;
