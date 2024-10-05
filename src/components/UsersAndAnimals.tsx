5d5adb11c1d01fcc98a7b44effb00ec422248e5b
Marek Včelák <vcelak.marek@gmail.com>
2024-10-05 21:52:59 +0200

    create UsersAndAnimals.tsx

---
+++ src/components/UsersAndAnimals.tsx
@@ -0,0 +1,16 @@
+import Button from "components/Button";
+
+const UsersAndAnimals = () => {
+  return (
+    <>
+      <Button color="grey" link="Users">
+        Users
+      </Button>
+      <Button color="grey" link="Animals">
+        Animals
+      </Button>
+    </>
+  );
+};
+
+export default UsersAndAnimals;
