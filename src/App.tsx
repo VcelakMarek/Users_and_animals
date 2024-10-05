752d1f8148f10db7134244a350a5b16a49d7ec2e
Marek Včelák <vcelak.marek@gmail.com>
2024-10-05 21:56:29 +0200

    add routes, queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import UsersAndAnimals from "components/UsersAndAnimals";
+import Users from "components/Users";
+import Animals from "components/Animals";
 
 const App = () => {
-  return;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="h-screen bg-light-bg">
+          <Routes>
+            <Route path="/Users_and_animals/" element={<UsersAndAnimals />} />
+            <Route path="/Users_and_animals/users/" element={<Users />} />
+            <Route path="/users/new" element={<NewUser />} />
+            <Route path="/Users_and_animals/animals/" element={<Animals />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 const container = document.getElementById("root");
