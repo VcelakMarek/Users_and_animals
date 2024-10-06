478a9dd974ae032aa1c204c4b53533c9f2ce3349
VcelakMarek <vcelak.marek@gmail.com>
2024-10-06 17:49:55 +0200

    create EditAnimal.tsx

---
+++ src/components/EditAnimal.tsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import { getAnimal } from "api/AnimalApi";
+import AnimalForm from "components/AnimalForm";
+
+const EditAnimal = () => {
+  const { id } = useParams();
+
+  if (!id) {
+    return <div>Invalid Animal ID</div>;
+  }
+
+  const { animalData, isFetching } = getAnimal(id);
+
+  if (isFetching) {
+    return (
+      <>
+        <div className="flex h-screen items-center justify-center">
+          <div className="h-20 w-20 animate-spin rounded-full border-8 border-gray-300 border-t-purple" />
+        </div>
+      </>
+    );
+  }
+
+  if (!animalData) {
+    return <p>Animal doesn`t exist</p>;
+  }
+  //error handling
+
+  return <AnimalForm formValues={animalData} animalId={id} />;
+};
+
+export default EditAnimal;
