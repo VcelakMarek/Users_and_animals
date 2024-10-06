d87d579444b701d6bb552a24b135b123fa94bc3c
VcelakMarek <vcelak.marek@gmail.com>
2024-10-06 17:33:22 +0200

    add getUser function

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -19,3 +19,26 @@ export const getUsers = () => {
 
   return { userList, isFetching, refetch, error };
 };
+
+export const getUser = (id: string) => {
+  const {
+    data: response,
+    isFetching,
+    isLoading,
+    refetch,
+    error,
+  } = useQuery({
+    queryKey: ["User"],
+    queryFn: () => GET<User>(`${url}/${id}`),
+    staleTime: 0, // Disable caching (always considered stale)
+    refetchOnWindowFocus: false, // Avoid refetching when window regains focus
+  });
+
+  const userData = response ? response.data : null;
+
+  return { userData, isFetching, isLoading, refetch, error };
+};
+
+export const updateUser = () => {};
+
+export const addUser = () => {};
