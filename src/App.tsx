b5c6f75044331198532db4abc5200b486be9749b
VcelakMarek <vcelak.marek@gmail.com>
2024-10-07 18:07:55 +0200

    install react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -46,6 +47,7 @@ const App = () => {
           </Routes>
         </div>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
