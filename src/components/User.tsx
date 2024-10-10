e735ca7f8171a4427aca67c22523bef792228da6
Marek Včelák <vcelak.marek@gmail.com>
2024-10-10 02:53:48 +0200

    add banUser, gender colors

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,11 +1,23 @@
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import Button from "components/Button";
 import AccessStatus from "components/AccessStatus";
-import { deleteUser } from "api/UserApi";
-import type { User } from "types/UserType";
+import { deleteUser, editUser } from "api/UserApi";
+import type { Gender, User } from "types/UserType";
 
 type UserProps = {
   userData: User;
+};
+
+const getGenderColor = (gender: Gender) => {
+  switch (gender) {
+    case "male":
+      return "text-blue-500";
+    case "female":
+      return "text-pink-500";
+    default:
+    case "other":
+      return "text-gray-500";
+  }
 };
 
 const User = ({ userData }: UserProps) => {
@@ -18,18 +30,33 @@ const User = ({ userData }: UserProps) => {
     },
   });
 
+  const { mutateAsync: editUserMutate } = useMutation({
+    mutationFn: editUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["Users"] });
+    },
+  });
+
   const handleDelete = async () => {
     await deleteUserMutate(userData.id);
+  };
+
+  const banUser = async () => {
+    await editUserMutate({ ...userData, banned: true });
   };
 
   return (
     <div className="mb-4 flex h-[72px] w-full items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px] sm:h-[300px] md:h-[200px] md:flex-wrap">
       <h4 className="basis-3/12 text-start">{userData.name}</h4>
-      <h3 className="basis-1/12">{userData.gender}</h3>
+      <h3
+        className={`basis-1/12 first-letter:uppercase ${getGenderColor(userData.gender)}`}
+      >
+        {userData.gender}
+      </h3>
 
-      <AccessStatus isBanned={userData.banned} />
+      <AccessStatus isBanned={userData.banned} onClick={banUser} />
 
-      <div className="flex basis-2/12 items-center gap-4">
+      <div className="flex basis-2/12 items-center gap-4 md:basis-1/2 md:justify-center">
         <Button color="grey" link={`edit/${userData.id}`}>
           Edit
         </Button>
