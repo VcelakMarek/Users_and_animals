c4364a11f1471275e09f827236daf1302fc12f65
Marek Včelák <vcelak.marek@gmail.com>
2024-10-10 23:30:16 +0200

    create gh-pages branch

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="h-20 w-20 animate-spin rounded-full border-8 border-gray-300 border-t-purple" />
-    </div>
-  );
-};
-
-export default Loading;
--- src/types/AnimalType.ts
+++
@@ -1,9 +0,0 @@
-export type Animal = {
-  id: string;
-  name: string;
-  type: "cat" | "dog" | "other";
-  age: number;
-};
-
-/** Type used for adding new Animal, API creates the id */
-export type AnimalNoId = Omit<Animal, "id">;
