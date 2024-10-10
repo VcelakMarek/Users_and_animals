5ab46d2b396eb4824abd1e8c97863f73692db4b0
Marek Včelák <vcelak.marek@gmail.com>
2024-10-10 02:49:45 +0200

    add button

--- src/components/AccessStatus.tsx
+++ src/components/AccessStatus.tsx
@@ -3,21 +3,30 @@ const accessStatus = {
   banned: ["bg-[#D53333]", "text-[#D53333]"],
 };
 
-type Props = {
+type AccessStatusProps = {
   isBanned: boolean;
+  onClick: () => void;
 };
 
-const AccessStatus = ({ isBanned }: Props) => {
+const AccessStatus = ({ isBanned, onClick }: AccessStatusProps) => {
   const status = isBanned ? "banned" : "allowed";
   const color = accessStatus[status];
 
   return (
-    <div className={`flex h-10 w-[104px] rounded-md bg-opacity-10 ${color[0]}`}>
-      <div className="m-auto flex items-center text-xs font-bold">
-        <div className={`mr-2 h-2 w-2 rounded-full opacity-100 ${color[0]}`} />
-        <p className={`pt-0.5 first-letter:uppercase ${color[1]}`}>{status}</p>
+    <button onClick={onClick}>
+      <div
+        className={`flex h-10 w-[104px] rounded-md bg-opacity-10 ${color[0]}`}
+      >
+        <div className="m-auto flex items-center text-xs font-bold">
+          <div
+            className={`mr-2 h-2 w-2 rounded-full opacity-100 ${color[0]}`}
+          />
+          <p className={`pt-0.5 first-letter:uppercase ${color[1]}`}>
+            {status}
+          </p>
+        </div>
       </div>
-    </div>
+    </button>
   );
 };
 
